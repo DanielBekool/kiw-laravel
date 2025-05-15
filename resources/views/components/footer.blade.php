f6381c2d5d1846247ded1d10adf084623b4b9ea9
Daniel <danielbekool@gmail.com>
2025-05-15 18:34:58 +0700

    footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,88 @@
+<!--Footer-->
+        <section id="footer" class="lg:py-30 py-18 bg-cover bg-[var(--color-transit)]" style="background-image:url('{{ asset('images/Footer.jpg') }}')">
+            <div class="flex lg:flex-col sm:flex-row overflow-hidden relative flex-col-reverse lg:gap-20 sm:gap-10 gap-10  lg:px-0 lg:max-w-[1200px] mx-auto sm:px-6 px-4">
+                <div>
+
+                    <div class="mb-15">
+                        <h2 class="!text-white w-[850px] footer">Mulai Investasi Anda di Kawasan Industri Strategis!</h2>
+                    </div>
+
+                    <div>
+                        <div class="flex flex-col gap-5">
+                            <a class="w-[500px] btn4 group" href="https://maps.app.goo.gl/ujmEMzm5t5cCsjGx9" target="_blank" rel="noopener noreferrer">
+                                <span class="transition-all duration-300
+                                            group-hover:text-transparent 
+                                            group-hover:bg-clip-text 
+                                            group-hover:[background-image:linear-gradient(268deg,#1F77D3_1.1%,#321B71_99.1%)]">
+                                    Jl. Raya Semarang, Kendal KM 12, Indonesia
+                                </span>
+
+                                <span class="gradient-icon group-hover:hidden">
+                                    <x-arrow-right-color/>
+                                </span>
+
+                                <span class="gradient-icon hidden group-hover:block">
+                                    <x-arrow-right-gradient/>
+                                </span>
+                            </a>
+
+                            <a class="w-[400px] btn4 group" href="mailto:pemasaran@kiw.co.id">
+                                <span class="transition-all duration-300
+                                            group-hover:text-transparent 
+                                            group-hover:bg-clip-text 
+                                            group-hover:[background-image:linear-gradient(268deg,#1F77D3_1.1%,#321B71_99.1%)]">
+                                    pemasaran@kiw.co.id
+                                </span>
+
+                                <span class="gradient-icon group-hover:hidden">
+                                    <x-arrow-right-color/>
+                                </span>
+
+                                <span class="gradient-icon hidden group-hover:block">
+                                    <x-arrow-right-gradient/>
+                                </span>
+                            </a>
+
+                            <a class="w-[250px] btn4 group" href="tel:+6281211118022">
+                                <span class="transition-all duration-300
+                                            group-hover:text-transparent 
+                                            group-hover:bg-clip-text 
+                                            group-hover:[background-image:linear-gradient(268deg,#1F77D3_1.1%,#321B71_99.1%)]">
+                                    +62 812 1111 8022
+                                </span>
+
+                                <span class="gradient-icon group-hover:hidden">
+                                    <x-arrow-right-color/>
+                                </span>
+
+                                <span class="gradient-icon hidden group-hover:block">
+                                    <x-arrow-right-gradient/>
+                                </span>
+                            </a>
+
+
+                        </div>
+                        <div>
+
+                        </div>
+                        <div>
+
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                </div>
+
+                <div>
+
+                </div>    
+            
+            
+            </div>
+        </section>
